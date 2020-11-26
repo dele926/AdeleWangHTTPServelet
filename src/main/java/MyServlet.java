058efe0b1df64dd05141c6e546876366952d48cc
dele926 <adelesunnywang@gmail.com>
2020-11-26 11:57:58 +0000

    LongHTML

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -20,7 +20,7 @@ public class MyServlet extends HttpServlet {
                 "\n" +
                 "<HEAD>\n" +
                 "\n" +
-                "<TITLE>Your Title Here</TITLE>\n" +
+                "<TITLE>Adele's Servlet</TITLE>\n" +
                 "\n" +
                 "</HEAD>\n" +
                 "\n" +
@@ -30,23 +30,23 @@ public class MyServlet extends HttpServlet {
                 "\n" +
                 "<HR>\n" +
                 "\n" +
-                "<a href=\"http://somegreatsite.com\">Link Name</a>\n" +
+                "<a href=\"http://google.com\">This Link</a>\n" +
                 "\n" +
                 "is a link to another nifty site\n" +
                 "\n" +
-                "<H1>This is a Header</H1>\n" +
+                "<H1>Welcome to  my Servlet!</H1>\n" +
                 "\n" +
-                "<H2>This is a Medium Header</H2>\n" +
+                "<H2>First Servlet of the year</H2>\n" +
                 "\n" +
                 "Send me mail at <a href=\"mailto:support@yourcompany.com\">\n" +
                 "\n" +
                 "support@yourcompany.com</a>.\n" +
                 "\n" +
-                "<P> This is a new paragraph!\n" +
+                "<P> hello there!\n" +
                 "\n" +
-                "<P> <B>This is a new paragraph!</B>\n" +
+                "<P> <B>morning!</B>\n" +
                 "\n" +
-                "<BR> <B><I>This is a new sentence without a paragraph break, in bold italics.</I></B>\n" +
+                "<BR> <B><I>this is a very important sentence.</I></B>\n" +
                 "\n" +
                 "<HR>\n" +
                 "\n" +
