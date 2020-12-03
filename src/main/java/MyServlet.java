886ab5014c82fb3c79d707f01dfe0b6cb0c779b3
dele926 <adelesunnywang@gmail.com>
2020-12-03 23:01:03 +0000

    PostMethod

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.stream.Collectors;
 
 
 @WebServlet(urlPatterns = {"/patients"},loadOnStartup = 1)
@@ -58,7 +59,10 @@ public class MyServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
+        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        resp.setContentType("text/html");
+        resp.getWriter().write("Thank you client! "+reqBody);
     }
 }
